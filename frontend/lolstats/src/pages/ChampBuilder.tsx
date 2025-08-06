353fcb6ee49c0154e5d65028cd00534bca6deb45
CoderCruz <cruzjosework@gmail.com>
2025-08-05 20:26:01 -0400

    filtered items from api to match those in summoners rift. Added input field to search for items. Added CSS and cleaned up file

--- frontend/lolstats/src/pages/ChampBuilder.tsx
+++ frontend/lolstats/src/pages/ChampBuilder.tsx
@@ -8,27 +8,43 @@ const ChampBuilder = () => {
   const imageBaseURL = 'https://ddragon.leagueoflegends.com/cdn/15.2.1/img/champion/';
   const itemBaseURL = 'https://ddragon.leagueoflegends.com/cdn/15.2.1/img/item/';
   const [champStats, setChampStats] = useState(state.stats);
-  const [items, setItems] = useState(null);
+  const [allItems, setAllItems] = useState([]);
+  const [items, setItems] = useState([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [userInput, setUserInput] = useState<String>("");
 
     
   const getItems = async () => {
     const items = await getItemData()
-    console.log(items)
-    setItems(Object.values(items));
+    const validItems = []
+    Object.values(items).forEach(item => {
+      if(item.maps?.["11"]) validItems.push(item)
+    })
+    setItems(validItems);
+    setAllItems(validItems);
     setLoading(false);
+  }
+
+  const filterItems = e => {
+    let input = e.target.value;
+    setUserInput(input)
+    const filtered = allItems.filter(item => 
+      item.name.toLowerCase().includes(input.toLowerCase())
+    )
+    setItems(filtered)
+    
+
   }
 
   
   useEffect(() => {
-    return async () => await getItems()
+    getItems()
   }, [])
 
-   console.log(items)
    if (loading) {
       return (
         <div className="min-h-screen w-full bg-gray-950 flex items-center justify-center text-stone-300 text-lg">
-          Loading champions...
+          Loading page...
         </div>
       );
     }
@@ -54,23 +70,34 @@ const ChampBuilder = () => {
         </div>
       </div>
 
-      {/* Bottom half: Items... Info needed; name, image, on hover -> description, cost*/}
+      
       <div className="h-1/2 mt-4 border-t border-stone-700 pt-4 overflow-y-auto">
-        <h2 className="text-lg font-semibold mb-2">Choose Items</h2>
-        <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-4">
-          {Array.isArray(items) && items.map((item, index) => (
-            <div
-              key={index}
-              className="flex flex-col items-center bg-stone-800 p-2 rounded hover:bg-stone-700 cursor-pointer transition"
-            >
-              <img
-                className="w-12 h-12 object-contain mb-1"
-                src={`${itemBaseURL}${item.image.full}`}
-                alt={item.name}
-              />
-              <span className="text-sm text-center">{item.name}</span>
-            </div>
-          ))}
+        <div className="flex px-4 justify-between items-center">
+          <h2 className="text-lg font-semibold">Choose Items</h2>
+          <input
+            type="text"
+            value={userInput}
+            onChange={filterItems}
+            placeholder="Search item..."
+            className="w-1/4 h-8 bg-stone-800 border border-stone-700 rounded-md px-4 py-2 text-stone-200 placeholder-stone-400 focus:outline-none focus:ring-2 focus:ring-stone-600"
+          />
+        </div>
+
+        <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-4 mt-4">
+          {Array.isArray(items) &&
+            items.map((item, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-center bg-stone-800 p-2 rounded hover:bg-stone-700 cursor-pointer transition"
+              >
+                <img
+                  className="w-12 h-12 object-contain mb-1"
+                  src={`${itemBaseURL}${item.image.full}`}
+                  alt={item.name}
+                />
+                <span className="text-sm text-center">{item.name}</span>
+              </div>
+            ))}
         </div>
       </div>
     </div>
