009f3f0bfd05cea77a3c15cc5912f0bd5b001b05
CoderCruz <cruzjosework@gmail.com>
2025-08-06 16:52:24 -0400

    fixed services

--- frontend/lolstats/src/services/lolData.ts
+++ frontend/lolstats/src/services/lolData.ts
@@ -6,15 +6,15 @@ const getVersion = async (): Promise<string> => {
   return res.data[0];
 };
 
-export const getLeagueData = async (): Promise<Champion[]> => {
+export const getLeagueData = async (): Promise<Record<string, Champion>> => {
   try {
     const version = await getVersion();
     const res = await axios.get(`https://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`);
     const championsMap = res.data.data;
-    return Object.values(championsMap) as Champ[];
+    return championsMap;
   } catch (error) {
     console.error(`ERROR in getLeagueData: ${error}`);
-    return [];
+    return {};
   }
 };
 
@@ -22,9 +22,9 @@ export const getChampData = async (name: string, champArray?: Champion[]): Promi
   if (champArray && champArray.length > 0) {
     return champArray.find(champ => champ.name === name);
   }
-
-  const champsList = await getLeagueData();
-  return champsList.find(champ => champ.name === name);
+  
+  const champsMap = await getLeagueData();
+  return Object.values(champsMap).find(champ => champ.name === name);
 };
 
 export const getItemData = async (): Promise<{ [key: string]: any }> => {
